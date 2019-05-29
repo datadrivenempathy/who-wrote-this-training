d0b00949e49a28a29fd7d5b99fb5cc85d103a609
A Pottinger <samp@gleap.org>
2019-05-29 15:38:12 -0700

    Updated links with predictions.

--- sql/links_with_predictions.sql
+++ sql/links_with_predictions.sql
@@ -20,27 +20,52 @@ SELECT
 FROM
 	(
 		SELECT
-			predictions.title AS title,
+			predictions_orig_title.title AS title,
 		    (
 		        CASE
 		            WHEN links_grouped.link isnull THEN ''
 		            ELSE links_grouped.link
 		        END
 		    ) AS link,
-			predictions.actualSource AS actualSource,
-			predictions.prediction AS predictedSource,
-			predictions.cnnScore AS cnnScore,
-			predictions.foxScore AS foxScore,
-			predictions.dailyMailScore AS dailyMailScore,
-			predictions.drudgeReportScore AS drudgeReportScore,
-			predictions.newYorkTimesScore AS newYorkTimesScore,
-			predictions.bbcScore AS bbcScore,
-			predictions.breitbartScore AS breitbartScore,
-			predictions.wallStreetJournalScore AS wallStreetJournalScore,
-			predictions.voxScore AS voxScore,
-			predictions.nprScore AS nprScore
+			predictions_orig_title.actualSource AS actualSource,
+			predictions_orig_title.prediction AS predictedSource,
+			predictions_orig_title.cnnScore AS cnnScore,
+			predictions_orig_title.foxScore AS foxScore,
+			predictions_orig_title.dailyMailScore AS dailyMailScore,
+			predictions_orig_title.drudgeReportScore AS drudgeReportScore,
+			predictions_orig_title.newYorkTimesScore AS newYorkTimesScore,
+			predictions_orig_title.bbcScore AS bbcScore,
+			predictions_orig_title.breitbartScore AS breitbartScore,
+			predictions_orig_title.wallStreetJournalScore AS wallStreetJournalScore,
+			predictions_orig_title.voxScore AS voxScore,
+			predictions_orig_title.nprScore AS nprScore
 		FROM
-			predictions
+			(
+				SELECT
+					transformation_bridge.originalTitle AS title,
+					predictions.actualSource AS actualSource,
+					predictions.prediction AS prediction,
+					predictions.cnnScore AS cnnScore,
+					predictions.foxScore AS foxScore,
+					predictions.dailyMailScore AS dailyMailScore,
+					predictions.drudgeReportScore AS drudgeReportScore,
+					predictions.newYorkTimesScore AS newYorkTimesScore,
+					predictions.bbcScore AS bbcScore,
+					predictions.breitbartScore AS breitbartScore,
+					predictions.wallStreetJournalScore AS wallStreetJournalScore,
+					predictions.voxScore AS voxScore,
+					predictions.nprScore AS nprScore
+				FROM
+					predictions
+				INNER JOIN
+					transformation_bridge
+				ON
+					transformation_bridge.newTitle = predictions.title
+					AND transformation_bridge.source = predictions.actualSource
+				GROUP BY
+					transformation_bridge.originalTitle,
+					predictions.actualSource
+			) predictions_orig_title
 		INNER JOIN
 		    (
 		        SELECT
@@ -54,8 +79,10 @@ FROM
 		            source
 		    ) links_grouped
 		ON
-		    predictions.title = links_grouped.title
-		    AND predictions.actualSource = links_grouped.source
+		    predictions_orig_title.title = links_grouped.title
+		    AND predictions_orig_title.actualSource = links_grouped.source
 	) joined
 WHERE
 	joined.predictedSource = joined.actualSource
+ORDER BY
+	score DESC, length(title) DESC
